116366a2a7b8ba4d28fa22ca6a945ff339714b3a
nodwon <nodwon5024@gmail.com>
2023-06-01 22:55:31 +0900

    JPA N+

--- src/main/java/jpabook/jpashop/JpabookApplication.java
+++ src/main/java/jpabook/jpashop/JpabookApplication.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.querydsl.jpa.Hibernate5Templates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -11,5 +14,12 @@ public class JpabookApplication {
     public static void main(String[] args) {
         SpringApplication.run(JpabookApplication.class, args);
     }
-
+    @Bean
+    Hibernate5Module hibernate5Module(){
+        //강제 지연 로딩 설정
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING,
+                true);
+        return hibernate5Module;
+    }
 }
---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,83 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.logging.SimpleFormatter;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+/*
+xToOne(Many toOne oneToone
+Order
+Order ->member
+Order -> Delivery
+* */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    /*
+    v1. 엔티티 직접 노출
+    하이버 네이트 모듈 등록 lazy = null cjfl
+    양방향 관계문제 발생 @jsonignore
+     */
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for(Order order:all){
+            order.getMember().getUsername(); // Lazy 강제 초기화
+            order.getDelivery().getAddress();// Lazy 강제 초기화
+        }
+        return  all;
+    }
+    //List가아니라 result로 한번 감싸야한다,
+    // 메이즈 테이블을 조회하는 거여서
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // N+1 -> 1 + 회원 N + 배송 N;
+
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
+    }
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getUsername();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+}
