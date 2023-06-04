cece4f649f6a1215fa1b6ddb814f6ad4c4722038
nodwon <nodwon5024@gmail.com>
2023-06-04 15:03:31 +0900

    API 개발 고급 - 6

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -87,15 +87,5 @@ public class OrderQueryRepository { //화면이나 의존관계를 떼어네기�
                 .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
     }
 
-    public List<OrderFlatDto> findAllByDto_flat() {
-        return  em.createQuery(
-                " select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.username, o.orderDate,o.status, d.address, i.name, oi.orderPrice, oi.count)" +
-                        " from Order o " +
-                        " join o.member m " +
-                        " join o.delivery d" +
-                        " join o.orderItems oi" +
-                        " join oi.item i", OrderFlatDto.class)
-                .getResultList();
 
-    }
 }
